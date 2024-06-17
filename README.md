bc4529715ddccf11619d50d029777f48fafd2c27
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2024-06-17 15:15:02 -0500

    Inicio del proyecto.

---
+++ README.md
@@ -0,0 +1,3 @@
+# tik_tok_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/alonsoam/tik_tok_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.alonsoam.tik_tok_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
